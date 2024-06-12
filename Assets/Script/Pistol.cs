46d52392896fff08d593d5a04be2772b7c7b3aab
DaveHagenauer <david.hagenauer05@gmail.com>
2024-06-12 09:00:22 +0200

    pistol funktioniert

--- Assets/Script/Pistol.cs
+++ Assets/Script/Pistol.cs
@@ -5,6 +5,7 @@ public class Pistol : MonoBehaviour
     public GameObject bullet;
     public Transform spawnPoint;
     public float bulletSpeed = 20f;
+    public int fireRate;
 
     public void FireBullet()
     {
@@ -13,8 +14,17 @@ public class Pistol : MonoBehaviour
         spawnedBullet.transform.position = spawnPoint.position;
         spawnedBullet.GetComponent<Rigidbody>()
             .AddForce(spawnPoint.forward * bulletSpeed, ForceMode.Impulse);
-
+        
         //--- Destroy the bullet after 5 seconds
         Destroy(spawnedBullet, 5);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FireBullet();
+
+        }
     }
 }
