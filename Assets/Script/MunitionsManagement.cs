b3dcb88abdfb66334eb4599eea16a6b775fdef2d
henrik0314 <heja.1709@gmail.com>
2024-06-19 08:14:45 +0200

    munition und MakeyMakey

--- Assets/Script/MunitionsManagement.cs
+++ Assets/Script/MunitionsManagement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MunitionsManagement : MonoBehaviour
 {
     public int munition;
+    public int munition02;
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +17,19 @@ public class MunitionsManagement : MonoBehaviour
     void Update()
     {
         munition = PlayerPrefs.GetInt("munition");
+        if (Input.GetMouseButtonDown(0))
+        {
+            munition--;
+            PlayerPrefs.SetInt("munition", munition);
+        }
                          
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (munition < 10)
             {
-                munition++;
-                PlayerPrefs.SetInt("munition", munition);
+                munition02 = munition;
+                munition02++;
+                PlayerPrefs.SetInt("munition", munition02);
                 Debug.Log("Munition gesendet");
             }
         }
